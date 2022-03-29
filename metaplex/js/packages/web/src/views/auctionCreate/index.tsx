0dc810dfa82f695e8fce2210d9583d2bd44575c2
MoneshROR <monesh.thoughtwin@gmail.com>
2022-03-29 19:45:26 +0530

    add auction create index file

--- metaplex/js/packages/web/src/views/auctionCreate/index.tsx
+++ metaplex/js/packages/web/src/views/auctionCreate/index.tsx
@@ -708,7 +708,8 @@ const CategoryStep = (props: {
       <Row className="call-to-action">
         <h2>List an item</h2>
         <p>
-          First time listing on Metaplex? <a>Read our sellers&apos; guide.</a>
+          First time listing on Thoughtwin marketplace?{' '}
+          <a>Read our sellers&apos; guide.</a>
         </p>
       </Row>
       <Row justify={width < 768 ? 'center' : 'start'}>
